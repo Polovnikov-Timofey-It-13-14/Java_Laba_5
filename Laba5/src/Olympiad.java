6f6100648906284c8a400f0b63c0a1ae0073e06a
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-12-07 16:01:21 +0500

    Update Olympiad.java

--- Laba5/src/Olympiad.java
+++ Laba5/src/Olympiad.java
@@ -1,65 +1,47 @@
+import java.nio.file.*;
 import java.util.*;
 
 public class Olympiad {
-    private Map<Integer, Participant> participants;
-    private int maxTotalScore;
+    private String bestLastName;
+    private String bestFirstName;
+    private int bestScore1;
+    private int bestScore2;
+    private int bestScore3;
+    private int bestTotalScore;
+    private String winnersList;
 
+    // Конструктор
     public Olympiad() {
-        this.participants = new HashMap<>();
-        this.maxTotalScore = 0;
+        this.bestTotalScore = 0;
+        this.winnersList = "";
     }
 
-    //Добавить участника
+    // Метод для добавления участника и обновления информации о победителях
     public void addParticipant(String lastName, String firstName, int score1, int score2, int score3) {
-        Participant participant = new Participant(lastName, firstName, score1, score2, score3);
-        int newIndex = participants.size();
-        participants.put(newIndex, participant);
+        int totalScore = calculateTotalScore(score1, score2, score3);
 
-        if (participant.getTotalScore() > maxTotalScore) {
-            maxTotalScore = participant.getTotalScore();
+        // Если это первый участник или у него больше баллов, чем у текущего лучшего
+        if (totalScore > bestTotalScore) {
+            // Обновляем данные лучшего участника
+            bestLastName = lastName;
+            bestFirstName = firstName;
+            bestScore1 = score1;
+            bestScore2 = score2;
+            bestScore3 = score3;
+            bestTotalScore = totalScore;
+
+            // Начинаем новый список победителей
+            winnersList = lastName + " " + firstName + "\n";
+        }
+        // Если у участника столько же баллов, как у лучшего
+        else if (totalScore == bestTotalScore) {
+            // Добавляем его к списку победителей
+            winnersList += lastName + " " + firstName + "\n";
         }
     }
 
-    //Поиск лучшего
-    public List<Integer> findBest() {
-        List<Integer> bestIndices = new ArrayList<>();
-        for (Map.Entry<Integer, Participant> entry : participants.entrySet()) {
-            if (entry.getValue().getTotalScore() == maxTotalScore) {
-                bestIndices.add(entry.getKey());
-            }
-        }
-        return bestIndices;
-    }
-
-    public int getParticipantsCount() {
-        return participants.size();
-    }
-
-    public int getMaxTotalScore() {
-        return maxTotalScore;
-    }
-
-    public String getLastName(int index) {
-        return participants.get(index).getLastName();
-    }
-
-    public String getFirstName(int index) {
-        return participants.get(index).getFirstName();
-    }
-
-    public int getScore1(int index) {
-        return participants.get(index).getScore1();
-    }
-
-    public int getScore2(int index) {
-        return participants.get(index).getScore2();
-    }
-
-    public int getScore3(int index) {
-        return participants.get(index).getScore3();
-    }
-
-    public int getTotalScore(int index) {
-        return participants.get(index).getTotalScore();
+    // Метод для вычисления общей суммы баллов
+    public int calculateTotalScore(int score1, int score2, int score3) {
+        return score1 + score2 + score3;
     }
 }
